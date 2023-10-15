0c783695aa5b1b56bb674b909ff7c368b3999d01
7sungcola <donghwa2428@gmail.com>
2023-10-16 02:25:48 +0900

    re-test for jenkins auto deploy

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('users.urls')),
     path('users', include('users.urls')),
     path('posts', include('posts.urls'))
 ]
