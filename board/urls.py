09bf26a8b986eb3a9bf30559a01b16b98d0a2496
7sungcola <donghwa2428@gmail.com>
2023-10-16 22:44:28 +0900

    jenkins execute shell issue

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('users.urls')),
     path('users', include('users.urls')),
     path('posts', include('posts.urls'))
 ]
