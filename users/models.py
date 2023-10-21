5e6e762f90930f3b6127bce46e8ec83ef3f7a0cc
7sungcola <donghwa2428@gmail.com>
2023-10-22 00:32:40 +0900

    default value added in users.model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db              import models
 from core.models import TimeStampModel
 
 class User(TimeStampModel):
-    name     = models.CharField(max_length=100)
+    name     = models.CharField(max_length=100, default='')
     email    = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
