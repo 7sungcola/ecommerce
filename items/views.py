536a0eef303ea0bc2ea778d2100436296c2fa4ba
7sungcola <donghwa2428@gmail.com>
2023-10-28 00:39:52 +0900

    items patch & delete added

--- items/views.py
+++ items/views.py
@@ -1,44 +1,93 @@
 import json
 
-from django.shortcuts import render
-from django.views     import View
-from django.http      import JsonResponse
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
 
-from .models          import Item, Category
-from core.utils       import authorization
+from .models                import Item, Category
+from core.utils             import authorization
 
 # Create your views here.
 class ItemView(View):
     def get(self, request):
-        data = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
 
-        name = data['name']
+            name = data['name']
 
-        item_found = Item.objects.get(name=name)
+            item_found = Item.objects.get(name=name)
 
-        return JsonResponse({'MESSAGE' : 'SUCCESS', 'RESULT' : item_found}, status=200)
+            return JsonResponse({'MESSAGE' : 'SUCCESS', 'RESULT' : item_found}, status=200)
+
+        except ValidationError as e:
+            return JsonResponse({'ERROR' : e.message}, status=400)
 
     @authorization
     def post(self, request):
-        data  = json.loads(request.body)
+        try:
+            data  = json.loads(request.body)
 
-        category  = data['category']
-        name      = data['name']
-        price     = data['price']
-        quantity  = data['quantity']
-        image_url = data['image_url']
+            category  = data['category']
+            name      = data['name']
+            price     = data['price']
+            quantity  = data['quantity']
+            image_url = data['image_url']
 
-        category = Category.objects.get(name=category)
+            category = Category.objects.get(name=category)
 
-        # if Category.objects.exists(category):
-        #     return JsonResponse({'ERROR' : 'Category already exists'}, status=400)
+            if Item.objects.exists(name):
+                return JsonResponse({'ERROR' : 'Item already exists'}, status=400)
 
-        Item.objects.create(
-            category_id = category.id,
-            name      = name,
-            price     = price,
-            quantity  = quantity,
-            image_url = image_url,
-        )
+            Item.objects.create(
+                category_id = category.id,
+                name      = name,
+                price     = price,
+                quantity  = quantity,
+                image_url = image_url,
+            )
 
-        return JsonResponse({'MESSAGE' : 'Items successfully added'}, status=200)
+            return JsonResponse({'MESSAGE' : 'Created'}, status=200)
+
+        except ValidationError as e:
+            return JsonResponse({'ERROR' : e.message}, status=400)
+
+    @authorization
+    def patch(self, request):
+        try:
+            data = json.loads(request.body)
+
+            item_name = data['name']
+
+            item = Item.objects.get(name=item_name)
+
+            item.name = data['name']
+            item.price = data['price']
+            item.quantity = data['quantity']
+            item.image_url = data['image_url']
+
+            item.save()
+
+            return JsonResponse({'MESSAGE' : 'Updated'}, status=200)
+
+        except ValidationError as e:
+            return JsonResponse({'ERROR' : e.message}, status=400)
+
+    @authorization
+    def delete(self, request):
+        try:
+            data = json.loads(request.body)
+
+            item_name = data['name']
+
+            item = Item.objects.filter(name=item_name)
+
+            if item.exists():
+                item.delete()
+
+                return JsonResponse({'MESSAGE': 'Deleted'}, status=200)
+
+            else:
+                return JsonResponse({'ERROR': 'Item does not exist'}, status=400)
+
+        except ValidationError as e:
+            return JsonResponse({'ERROR' : e.message}, status=400)
