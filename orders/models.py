25230b0b7709ee2658e9eca8ca234b10411e5a57
7sungcola <donghwa2428@gmail.com>
2023-11-02 01:22:40 +0900

    modification in orders model

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,11 @@
-from django.db   import models
+from django.db    import models
 
-from core.models import TimeStampModel
+from users.models import User
+from items.models import Item
+from core.models  import TimeStampModel
 
 class Order(TimeStampModel):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=500)
     order_number = models.CharField(max_length=150, unique=True)
     order_status = models.ForeignKey('OrderStatus', on_delete=models.CASCADE)
@@ -13,6 +15,17 @@ class Order(TimeStampModel):
 
     def __str__(self):
         return self.order_number
+
+class OrderItem(TimeStampModel):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        db_table = 'order_items'
+
+    def __str__(self):
+        return self.item
 
 class OrderStatus(models.Model):
     status = models.CharField(max_length=10)
